392256328e3a971ba8488ce9556bd457b3cf1d7f
lucaskrespi <lucas.krespi@gmail.com>
2020-08-04 14:49:35 -0400

    background test

--- GAME1017_Template_W01/Background.h
+++ GAME1017_Template_W01/Background.h
@@ -2,7 +2,7 @@
 #include "Sprite.h"
 class Background : public Sprite {
 public:
-	Background(SDL_Rect s, SDL_FRect d, const char* p, std::string k, int velocity) : Sprite(s, d, p, k) 
+	Background(SDL_Rect s, SDL_FRect d, const char* p, std::string k, float velocity) : Sprite(s, d, p, k) 
 	{
 		m_velocity = velocity;
 	}
